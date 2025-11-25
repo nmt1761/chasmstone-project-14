0ecff066982767d5589be6a99dcecfd3d5714ef5
nmt1761 <69991207+nmt1761@users.noreply.github.com>
2025-11-25 12:56:22 -0500

    add fragmentation protocol

--- 1algorithm.c
+++ 1algorithm.c
@@ -1,7 +1,16 @@
-B = 1 // bandwidth from pre-configured C-V2X settings
-TC = 1 // certificate transmission interval (defaults to 500 ms).
-TM = 1 // BSM interval (defaults to 100 ms).
-HC = 1 // current certificate
+#include <math.h>
+
+B = 1; // bandwidth from pre-configured C-V2X settings
+TC = 500; // certificate transmission interval in ms (default)
+TM = 100; // BSM interval in ms (default)
+HC = 1; // current certificate
+HdrSec = 1; // IEEE 1609.2 security headers
+M = 1; // current BSM Message
+SigC = 1; // classic signature
+SigQR = 1; // quantum resistent signature
+q, r = 1; // sourced from MCS (Modulation and Coding Scheme)
+Nrb = 1; // number of Resource Blocks ? may depend on bandwidth
+HCid = 1; //authenticated certificate
 
 /*
   Pseduocode below huzzah!
@@ -11,9 +20,33 @@ HC = 1 // current certificate
   max|TB| = 12 * 10 * log2(q) * r * (Nrb - 2) * 1/8
   max|HCf| = max|TB| - min|S|
   nf = ceilingFunction(|HC|/max|HCf|)
+*/
 
 
-  while the current certificate is active do:
+
+  list FRAGMENT(HCid, q, r, B) {
+    
+    // Calculating some variables needed for fragmentation function
+    // minimum size of an SPDU
+    minS = sizeof(HdrSec) + sizeof(M) + sizeof(SigC) + sizeof(SigQR);
+    
+    // maximum size of transport block given our MCS 
+    maxTB  = 12 * 10 * log2(q) * r * (Nrb - 2) * 1/8;
+  
+    // maxmimum size of HCf (certificate fragment)
+    maxHCf = sizeof(maxTB) - sizeof(min); //possibly check if double sizeof causes error
+  
+    // number of certificate fragments
+    nf = ceilingFunction(sizeof(HC)/(sizeof(maxHCf);
+
+    // from here you divide HCid by nf in a way in which preserves the content/uses B (bandwidth) in some way?
+{HC1, ..., HCnf} = 0; 
+    
+    return (nf, {HC1, ..., HCnf});
+  
+
+/*
+  while HCid (the current certificate) is active do:
   select q (modulation order) and r (code rate) values (sourced from MCS to be used)
   nf = 0 // number of fragments
   
