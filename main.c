17ef723e6ffb24c92a692c5f7e891d8f309fef29
William Boylin <wdboylin@gmail.com>
2025-12-02 20:43:59 +0000

    commenting fragmenting test

--- main.c
+++ main.c
@@ -3,11 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+// retains a single fragment and its length
 typedef struct {
 	size_t fragmentLen;
 	char *fragmentString;
 } fragment;
 
+
+// manages a set number of fragments
 typedef struct {
 	size_t fragmentCount;
 	fragment *fragments;
@@ -18,39 +22,29 @@ typedef struct {
 
 
 int main() {
+
+	// set constant sizes for test
 	size_t count = 3;
 	size_t fragLen = 3;
 
-
+	// create fragment holder and allocate space for as many fragments as was specified
 	fragmentHolder *fragmentHold = malloc(sizeof(fragmentHolder));
 	fragmentHold->fragmentCount = count;
 	fragmentHold->fragments = calloc(count, sizeof(fragment));
 
-	/*
-	fragment *fragment1 = malloc(sizeof(fragment));
-	fragment *fragment2 = malloc(sizeof(fragment));
-	fragment *fragment3 = malloc(sizeof(fragment));
-
-	fragment1->fragmentLen = fragLen;
-	fragment2->fragmentLen = fragLen;
-	fragment3->fragmentLen = fragLen;
-	fragment1->fragmentString = "\x01\x01\x01";
-	fragment2->fragmentString = "\x02\x02\x02";
-	fragment3->fragmentString = "\x03\x03\x03";
-
-	fragmentHold->fragments[0] = *fragment1;
-	fragmentHold->fragments[1] = *fragment2;
-	fragmentHold->fragments[2] = *fragment3;
-	*/
+	// populate fragment objects with their strings and lengths
 	fragmentHold->fragments[0].fragmentLen = fragLen;
 	fragmentHold->fragments[0].fragmentString = "\x01\x01\x01";
-	fragmentHold->fragments[1].fragmentString = "\x02\x02\x02";
 	fragmentHold->fragments[1].fragmentLen = fragLen;
-	fragmentHold->fragments[2].fragmentString = "\x03\x03\x03";
+	fragmentHold->fragments[1].fragmentString = "\x02\x02\x02";
 	fragmentHold->fragments[2].fragmentLen = fragLen;
+	fragmentHold->fragments[2].fragmentString = "\x03\x03\x03";
 
+	// iteratively go through each fragment in the holder and print
 	for (int i = 0; i < fragmentHold->fragmentCount; i++) {
 		char *str = (char *)fragmentHold->fragments[i].fragmentString;
+
+			// iteratively print each value in a fragment
 			for (int j = 0; j < fragmentHold->fragments[0].fragmentLen; j++) {
 				printf("%02X ", str[j]);
 			}
